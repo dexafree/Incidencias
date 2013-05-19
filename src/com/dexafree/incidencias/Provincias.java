d9415237ec4d7bba6440e181735b7202d669e6bb
Dexafree <dexafree90@gmail.com>
2013-05-19 04:04:26 +0200

    Listado de provincias añadido + selector de provincia (no implementado todavía)

--- src/com/dexafree/incidencias/Provincias.java
+++ src/com/dexafree/incidencias/Provincias.java
@@ -2,6 +2,8 @@ package com.dexafree.incidencias;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.widget.AbsListView;
+import android.widget.ListAdapter;
 
 /**
  * Created by Carlos on 19/05/13.

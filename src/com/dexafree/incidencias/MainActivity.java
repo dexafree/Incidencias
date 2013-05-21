41a11d12f62ab7fcaffbe3fa0c8a589ae9020827
Dexafree <dexafree90@gmail.com>
2013-05-22 01:28:35 +0200

    BETA 1

--- src/com/dexafree/incidencias/MainActivity.java
+++ src/com/dexafree/incidencias/MainActivity.java
@@ -1,5 +1,8 @@
 package com.dexafree.incidencias;
 
+/**
+ * Created by Carlos on 21/05/13.
+ */
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -35,49 +38,45 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import com.dexafree.incidencias.CardStack;
+import com.dexafree.incidencias.CardUI;
+
 public class MainActivity extends Activity {
 
-    ListView lv1;
     ProgressDialog ShowProgress;
     public ArrayList<Incidencia> IncidenciaList = new ArrayList<Incidencia>();
+    private CardUI mCardView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
- 
-  /*      SharedPreferences pref =
-                PreferenceManager.getDefaultSharedPreferences(
-                        MainActivity.this);
-        Log.i("", "Opcion: " + pref.getBoolean("provincias_sel", false));*/
+        setContentView(R.layout.activity_main_cards);
 
 
 
+        // init CardView
+        mCardView = (CardUI) findViewById(R.id.cardsview);
+        mCardView.setSwipeable(true);
 
-        lv1 = (ListView) findViewById(R.id.listView1);
+
+        //TAREA DE CARGA DE XML Y PARSEO
 
         ShowProgress = ProgressDialog.show(MainActivity.this, "",
                 "Cargando. Espere por favor...", true);
         new loadingTask().execute("http://dgt.es/incidencias.xml");
-
-        lv1.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-
-                Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri
-                        .parse(IncidenciaList.get(position).getFechahora())); // AQUI IBA GETURL, PERO NO SE QUE HACE!
-                startActivity(intent);
-
-            }
-        });
-
-
-
-
+        firstTime();
 
     }
 
 
+    //MENU ACTIONBAR
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -86,7 +85,7 @@ public class MainActivity extends Activity {
     }
 
 
-
+    //GESTIONAR CLICK ACTIONBAR
     //GESTIONANDO EL CLICK DE LA ACTIONBAR
 
     @Override
@@ -101,19 +100,18 @@ public class MainActivity extends Activity {
                 return true;
             case R.id.actualizar:
                 actualizar();
-                Toast.makeText(getApplicationContext(), "Actualizado", Toast.LENGTH_LONG).show();
+
                 return true;
-            case R.id.probando:
-                //prueba();
-                //Toast.makeText(getApplicationContext(), "Hecho", Toast.LENGTH_LONG).show();
-                startActivity(new Intent(this, MainActivityProceso.class));
-                return true;
+          /*  case R.id.probando:
+                prueba();
+                Toast.makeText(getApplicationContext(), "Hecho", Toast.LENGTH_LONG).show();
+                return true; */
             case R.id.action_acerca:
                 startActivity(new Intent(this, About.class));
                 return true;
-            case R.id.vista_cartas:
+       /*     case R.id.vista_cartas:
                 startActivity(new Intent(this, CardsMainActivity.class));
-                return true;
+                return true; */
 
             default:
                 return super.onOptionsItemSelected(item);
@@ -121,7 +119,31 @@ public class MainActivity extends Activity {
     }
 
 
+    public void firstTime() {
+        final String PREFS_NAME = "MyPrefsFile";
 
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+
+        if (settings.getBoolean("my_first_time", true)) {
+            //the app is being launched for first time, do something
+            Log.d("Comments", "First time");
+
+            mCardView.addCard(new MyCard("No tienes ninguna provincia seleccionada", "Entra al menu de Provincias y selecciona la/s que te interese/n\n \nLuego pulsa Actualizar"));
+            mCardView.addCard(new MyCard("Puedes deslizar las tarjetas hacia los laterales", "Adelante, pruebalo!"));
+            mCardView.refresh();
+
+
+
+            // first time task
+
+            // record the fact that the app has been started at least once
+            settings.edit().putBoolean("my_first_time", false).commit();
+        }
+
+    }
+
+
+    //LOADINGTASK Y PARSEO
     class loadingTask extends AsyncTask<String, Void, String> {
 
         protected String doInBackground(String... urls) {
@@ -138,11 +160,14 @@ public class MainActivity extends Activity {
         }
 
         protected void onPostExecute(String s) {
-            lv1.setAdapter(new EfficientAdapter(MainActivity.this, IncidenciaList));
+            //   lv1.setAdapter(new EfficientAdapter(MainActivity.this, IncidenciaList));
             ShowProgress.dismiss();
+            mCardView.refresh();
 
         }
     }
+
+
 
     class SAXHelper {
         public HashMap<String, String> userList = new HashMap<String, String>();
@@ -169,23 +194,18 @@ public class MainActivity extends Activity {
     }
 
     public void actualizar() {
+        //ELIMINAMOS LAS INCIDENCIAS EXISTENTES
         IncidenciaList.clear();
-        lv1 = (ListView) findViewById(R.id.listView1);
+        mCardView.clearCards();
 
+        //CARGAMOS NUEVAS INCIDENCIAS
         ShowProgress = ProgressDialog.show(MainActivity.this, "",
                 "Cargando. Espere por favor...", true);
         new loadingTask().execute("http://dgt.es/incidencias.xml");
 
-        lv1.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-
-                Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri
-                        .parse(IncidenciaList.get(position).getFechahora())); // AQUI IBA GETURL, PERO NO SE QUE HACE!
-                startActivity(intent);
-
-            }
-        });
+        //REFRESCAR LA VISTA DE LAS CARDS
+        mCardView.refresh();
+        Toast.makeText(getApplicationContext(), "Actualizado", Toast.LENGTH_LONG).show();
 
     }
 
@@ -194,14 +214,14 @@ public class MainActivity extends Activity {
         SharedPreferences pref =
                 PreferenceManager.getDefaultSharedPreferences(
                         MainActivity.this);
-        if ( pref.getBoolean("testeo", false)) {
+      /*  if ( pref.getBoolean("testeo", false)) {
             Log.i("", "Es true");
 
         }
         else {
             Log.i("", "Es false");
 
-        }
+        }*/
 
         Date cDate = new Date();
         String fDate = new SimpleDateFormat("yyyy-MM-dd").format(cDate);
@@ -216,11 +236,11 @@ public class MainActivity extends Activity {
                         MainActivity.this);
 
         if ( pref.getBoolean("testeo", false)) {
-            Log.i("", "Es true");
+            // Log.i("", "Es true");
             return true;
         }
         else {
-            Log.i("", "Es false");
+            //Log.i("", "Es false");
             return false;
         }
 
@@ -230,8 +250,6 @@ public class MainActivity extends Activity {
 
     public boolean comparaFecha(String fechahora){
 
-
-
         Date cDate = new Date();
         String fDate = new SimpleDateFormat("yyyy-MM-dd").format(cDate);
 
@@ -240,42 +258,42 @@ public class MainActivity extends Activity {
         String month = new SimpleDateFormat("MM").format(cDate);
         String day = new SimpleDateFormat("dd").format(cDate);
 
-        Log.i("", "year month day: " + year + " " + month + " " + day);
+        //   Log.i("", "year month day: " + year + " " + month + " " + day);
 
         String yearpas = fechahora.substring(0,4);
         String monthpas = fechahora.substring(5,7);
         String daypas = fechahora.substring(8,10);
 
-        Log.i("", "yearpas monthpas daypas: " + yearpas + " " + monthpas + " " + daypas);
+        //  Log.i("", "yearpas monthpas daypas: " + yearpas + " " + monthpas + " " + daypas);
 
         if (year.equals(yearpas))
-         {
+        {
 
-             Log.i("", "COINCIDE YEAR");
+            //  Log.i("", "COINCIDE YEAR");
 
             if (month.equals(monthpas))
+            {
+                //  Log.i("", "COINCIDE MONTH");
+                if (day.equals(daypas))
                 {
-                    Log.i("", "COINCIDE MONTH");
-                    if (day.equals(daypas))
-                     {
-                         Log.i("", "COINCIDE DAY");
-                        return true;
-                     }
-                    else
-                     {
-                        return false;
-                     }
+                    //    Log.i("", "COINCIDE DAY");
+                    return true;
                 }
-            else
+                else
                 {
                     return false;
                 }
-         }
+            }
+            else
+            {
+                return false;
+            }
+        }
         else
-         {
-             Log.i("", "No coincide year");
-          return false;
-         }
+        {
+            //   Log.i("", "No coincide year");
+            return false;
+        }
 
 
     }
@@ -295,20 +313,33 @@ public class MainActivity extends Activity {
         return true;
     }
 
+    public String getHora(String fechaHora) {
+
+        Log.i("", "FechaHora: " + fechaHora);
+        String fhs = fechaHora.trim();
+        Log.i("", "fhs: " + fhs);
+        String hora = fhs.substring(11,13);
+        Log.i("", "hora: " + hora);
+        String minutos = fhs.substring(14,16);
+        Log.i("", "minutos: " + minutos);
+        return hora + ":" + minutos + "  ";
+
+    }
+
     public boolean checkProvincia(String provincia) {
 
         SharedPreferences pref =
                 PreferenceManager.getDefaultSharedPreferences(
                         MainActivity.this);
 
-        Log.i("", provincia + ": " + pref.getBoolean(provincia, false));
+        //   Log.i("", provincia + ": " + pref.getBoolean(provincia, false));
 
         if ( pref.getBoolean(provincia, false) == true) {
-            Log.i("", "Es true");
+            //   Log.i("", "Es true");
             return true;
         }
         else {
-            Log.i("", "Es false");
+            // Log.i("", "Es false");
             return false;
         }
     }
@@ -390,24 +421,27 @@ public class MainActivity extends Activity {
 
 
 
-               // Log.i("", "Funciona: " + currentIncidencia.getProvincia());
+                // Log.i("", "Funciona: " + currentIncidencia.getProvincia());
                 if (checkProvincia(currentIncidencia.getProvincia()) == true)
                 {
 
                     if (checkTesteo() == false)
                     {
-                         //   Log.i("", "Pasado el primer if");
-                       if (comparaFecha(currentIncidencia.getFechahora().trim()) == true)
+                        //   Log.i("", "Pasado el primer if");
+                        if (comparaFecha(currentIncidencia.getFechahora().trim()) == true)
 
-                         {
-                             //     Log.i("", "Añadida la provincia: " + currentIncidencia.getProvincia());
-                             IncidenciaList.add(currentIncidencia);
-                         }
+                        {
+                            //     Log.i("", "Añadida la provincia: " + currentIncidencia.getProvincia());
+                            IncidenciaList.add(currentIncidencia);
+                            mCardView.addCard(new MyCard(getHora(currentIncidencia.getFechahora()) + currentIncidencia.getCarretera() + "  -  " + currentIncidencia.getPoblacion(), currentIncidencia.getPoblacion(), "PK INICIAL: " + currentIncidencia.getPkInicio(), "PK FINAL: " + currentIncidencia.getPkFin()));
+                        }
 
                     }
                     else
                     {
                         IncidenciaList.add(currentIncidencia);
+                        mCardView.addCard(new MyCard(getHora(currentIncidencia.getFechahora()) + currentIncidencia.getCarretera() + "  -  " + currentIncidencia.getPoblacion(), currentIncidencia.getPoblacion(), "PK INICIAL: " + currentIncidencia.getPkInicio(), "PK FINAL: " + currentIncidencia.getPkFin()));
+
                     }
 
                 }
@@ -424,5 +458,6 @@ public class MainActivity extends Activity {
         }
 
     }
+
 
 }

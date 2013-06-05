0d27efada287e184c8177111988d6d99cbb1b6af
Dexafree <dexafree90@gmail.com>
2013-06-06 00:08:13 +0200

    VERSION FINAL

--- src/com/dexafree/incidencias/UpdateService.java
+++ src/com/dexafree/incidencias/UpdateService.java
@@ -4,6 +4,7 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -130,11 +131,16 @@ public class UpdateService extends Service {
                     builder.setWhen(System.currentTimeMillis());
                     builder.setDefaults(Notification.DEFAULT_ALL);
                     builder.setAutoCancel(true);
+                    builder.setLights(0xFF00FF00,500,500);
                     Intent i = new Intent(getApplicationContext(), MainFavoritos.class);
                     //i.putExtra("jsonFile", json.getJsonFile());
                     PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
                     builder.setContentIntent(pi);
                     nm.notify(0, builder.build());
+                    Intent intent = new Intent("com.dexafree.incidencias.NEW_NOTIFICATION");
+                    intent.putExtra("NEW_NOTIFICATION_TEXT", "Se han encontrado incidencias");
+                    Context context = getApplicationContext();
+                    context.sendBroadcast(intent);
                     Log.d("NOTIFICACION","Notificacion mostrada");
                 }
 
@@ -143,6 +149,8 @@ public class UpdateService extends Service {
         updateThread.start();
         super.onStart(intent, startId);
     }
+
+
 
     class loadingTask3 extends AsyncTask<String, Void, String> {
 

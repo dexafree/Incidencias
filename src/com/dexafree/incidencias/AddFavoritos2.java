b5a7d47fb76282f63f52294ad520cc29279395e7
Dexafree <dexafree90@gmail.com>
2014-01-01 22:55:31 +0100

    3.1 + Solucionado ParseIntError al no introducir numeros en los Favoritos + Corregido problema de color en Feedback

--- src/com/dexafree/incidencias/AddFavoritos2.java
+++ src/com/dexafree/incidencias/AddFavoritos2.java
@@ -106,113 +106,125 @@ public class AddFavoritos2 extends SherlockFragmentActivity implements OnPrefere
         btnGuardar.setOnClickListener(new OnClickListener() {
             public void onClick(View arg0){
 
-//                    Favoritos currentFavorito = new Favoritos();
 
                 carretera = sp.getString("carretera_seleccionada", "None Selected");
 
                 String pkInTemp = ETpkInicial.getText().toString();
-                pkInicial = Integer.parseInt(pkInTemp);
-
                 String pkFinTemp = ETpkFinal.getText().toString();
-                pkFinal = Integer.parseInt(pkFinTemp);
 
-                Log.d("", "Carretera: " + carretera);
+                if(pkInTemp.length() != 0 && pkFinTemp.length() != 0){
+                    pkInicial = Integer.parseInt(pkInTemp);
 
+                    pkFinal = Integer.parseInt(pkFinTemp);
 
-                currentFavorito.setCarretera(carretera);
-                currentFavorito.setPkInicial(pkInicial);
-                currentFavorito.setPkFinal(pkFinal);
-                currentFavorito.setTipo(2);
+                    Log.d("", "Carretera: " + carretera);
 
 
-                Favoritos.FavoritosList.add(currentFavorito);
+                    currentFavorito.setCarretera(carretera);
+                    currentFavorito.setPkInicial(pkInicial);
+                    currentFavorito.setPkFinal(pkFinal);
+                    currentFavorito.setTipo(2);
 
-                try
-                {
+
+                    Favoritos.FavoritosList.add(currentFavorito);
+
                     try{
-                        BufferedReader fin =
-                                new BufferedReader(
-                                        new InputStreamReader(
-                                                openFileInput("Favoritos.xml")));
+                        try{
+                            BufferedReader fin =
+                                    new BufferedReader(
+                                            new InputStreamReader(
+                                                    openFileInput("Favoritos.xml")));
 
-                        Log.d("","Archivo abierto");
+                            Log.d("","Archivo abierto");
 
-                        int lines = load();
+                            int lines = load();
 
-                        previo = fin.readLine();
-                        for(int i=1; i<lines; i++){
-                            previo = previo + "\n" + fin.readLine();
-                            Log.d("","previo: " + previo);
+                            previo = fin.readLine();
+                            for(int i=1; i<lines; i++){
+                                previo = previo + "\n" + fin.readLine();
+                                Log.d("","previo: " + previo);
+                            }
+
+                            fin.close();
                         }
 
-                        fin.close();
-                    }
+                        catch (Exception ex)
+                        {
 
-                    catch (Exception ex)
-                    {
+                            Log.e("XmlTips", "No existe el xml. Creando uno");
+                            OutputStreamWriter fcrear =
+                                    new OutputStreamWriter(
+                                            openFileOutput("Favoritos.xml", Context.MODE_PRIVATE));
+                            fcrear.close();
 
-                        Log.e("XmlTips", "No existe el xml. Creando uno");
-                        OutputStreamWriter fcrear =
+                        }
+
+                        //Creamos un fichero en la memoria interna
+                        Log.d("","Sale del primer try");
+                        OutputStreamWriter fout =
                                 new OutputStreamWriter(
                                         openFileOutput("Favoritos.xml", Context.MODE_PRIVATE));
-                        fcrear.close();
 
+                        StringBuilder sb = new StringBuilder();
+
+                        Log.d("","Consigue crear el sb");
+
+
+                        Log.d("", "Previo antes del if de previo: " + previo);
+                        //Construimos el XML
+                        if (previo != null){
+
+                            // char ar[] = previo.toCharArray();
+                            sb.append(previo + "\n");
+                        }
+
+                        Log.d("","Pasa el if");
+                        Log.d("","Datos: " + carretera + "  " + pkInicial + "  " + pkFinal);
+                        sb.append("<favorito>");
+                        sb.append("<tipo>" + 2 + "</tipo>");
+                        sb.append("<carretera>" + carretera + "</carretera>");
+                        sb.append("<provincia>" + "</provincia>");
+                        sb.append("<pkInicial>" + pkInicial + "</pkInicial>");
+                        sb.append("<pkFinal>" + pkFinal+ "</pkFinal>");
+                        sb.append("</favorito>");
+
+                        Log.d("", "sb antes de append: " + sb.toString());
+
+
+
+                        //Escribimos el resultado a un fichero
+                        fout.append(sb.toString());
+
+                        fout.close();
+
+                        Log.i("XmlTips", "Fichero XML creado correctamente.");
+
+                        Context context = getApplicationContext();
+                        CharSequence text = "Favorito añadido con la carretera " + carretera + ", km inicial " + pkInicial + ", km final " + pkFinal;
+                        int duration = Toast.LENGTH_SHORT;
+
+                        finish();
+
+                        ManageFavoritos.mf_lv.setAdapter((new FavoritosAdapter(AddFavoritos2.this)));
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                    catch (Exception ex){
+                        Log.e("XmlTips", "Error al escribir fichero XML.");
                     }
 
-                    //Creamos un fichero en la memoria interna
-                    Log.d("","Sale del primer try");
-                    OutputStreamWriter fout =
-                            new OutputStreamWriter(
-                                    openFileOutput("Favoritos.xml", Context.MODE_PRIVATE));
-
-                    StringBuilder sb = new StringBuilder();
-
-                    Log.d("","Consigue crear el sb");
-
-
-                    Log.d("", "Previo antes del if de previo: " + previo);
-                    //Construimos el XML
-                    if (previo != null){
-
-                        // char ar[] = previo.toCharArray();
-                        sb.append(previo + "\n");
-                    }
-
-                    Log.d("","Pasa el if");
-                    Log.d("","Datos: " + carretera + "  " + pkInicial + "  " + pkFinal);
-                    sb.append("<favorito>");
-                    sb.append("<tipo>" + 2 + "</tipo>");
-                    sb.append("<carretera>" + carretera + "</carretera>");
-                    sb.append("<provincia>" + "</provincia>");
-                    sb.append("<pkInicial>" + pkInicial + "</pkInicial>");
-                    sb.append("<pkFinal>" + pkFinal+ "</pkFinal>");
-                    sb.append("</favorito>");
-
-                    Log.d("", "sb antes de append: " + sb.toString());
 
 
 
-                    //Escribimos el resultado a un fichero
-                    fout.append(sb.toString());
 
-                    fout.close();
-
-                    Log.i("XmlTips", "Fichero XML creado correctamente.");
+                } else {
 
                     Context context = getApplicationContext();
-                    CharSequence text = "Favorito añadido con la carretera " + carretera + ", km inicial " + pkInicial + ", km final " + pkFinal;
+                    CharSequence text = "Asegurate de rellenar todos los campos antes de darle a guardar!";
                     int duration = Toast.LENGTH_SHORT;
-
-                    finish();
-
-                    ManageFavoritos.mf_lv.setAdapter((new FavoritosAdapter(AddFavoritos2.this)));
-                    Toast toast = Toast.makeText(context, text, duration);
-                    toast.show();
+                    Toast.makeText(context, text, duration).show();
                 }
-                catch (Exception ex)
-                {
-                    Log.e("XmlTips", "Error al escribir fichero XML.");
-                }
+
             }
         });
 
